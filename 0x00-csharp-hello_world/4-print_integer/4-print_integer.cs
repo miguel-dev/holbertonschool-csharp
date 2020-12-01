4b13ce40a9fa4b2c37b8c9e2acec643e38f94649
miguel-dev <miguelacortes@zoho.com>
2020-12-01 10:11:45 -0500

    [Fixes] Added $ before the string to facilitate substitution

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		int number = 972;
-		Console.WriteLine("{number} Mission Street", number);
+		Console.WriteLine($"{number} Mission Street", number);
 	}
  }
