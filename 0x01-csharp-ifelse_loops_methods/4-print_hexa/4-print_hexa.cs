1b77ced4d77f40413a7d7b6e6b6409b2fce73c42
miguel-dev <miguelacortes@zoho.com>
2020-12-02 13:26:01 -0500

    [Task] 4. Hexadecimal printing

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	for (int i = 0; i < 99; i++)
+	{
+            Console.WriteLine("{0} = 0x{1}", i, Convert.ToString(i, 16));
+	}
+    }
+}
