57f7704e574c6863bb276415b0bc4dc92ac57ae0
miguel-dev <miguelacortes@zoho.com>
2020-12-02 14:33:47 -0500

    [Task] 5. 00...99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+	{
+	    if (i < 10)
+            {
+	        Console.Write($"0{i}");
+	    }
+	    else
+	    {    
+                Console.Write(i);
+	    }
+
+	    if (i < 99)
+            {
+                Console.Write(", ");
+            }
+	    else
+	    {
+                Console.WriteLine();
+            }
+	}
+    }
+}
