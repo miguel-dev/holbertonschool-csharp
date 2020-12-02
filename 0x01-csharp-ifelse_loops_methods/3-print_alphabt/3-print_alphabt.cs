46ea75f03370825d082dc60d36c05bbd335227b3
miguel-dev <miguelacortes@zoho.com>
2020-12-02 12:58:50 -0500

    [Task] 3. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char a = 'a'; a <= 'z'; a++)
+	{
+            if (a != 'q' && a != 'e')
+	    {
+                Console.Write(a);
+	    }
+        }
+    }
+}
