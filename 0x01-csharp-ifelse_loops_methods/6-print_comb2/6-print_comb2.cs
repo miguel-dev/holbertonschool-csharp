77ab192000298e554c5fca446b329aedd7578ef6
miguel-dev <miguelacortes@zoho.com>
2020-12-02 17:35:45 -0500

    [Task] 6. Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+	for (int a = 0; a < 10; a++)
+	{
+            for (int b = 0; b < 10; b++)
+	    {
+	        if (a != b && b > a)
+		{
+                    Console.Write($"{a}{b}", a, b);
+		    if (a != 8 || b != 9)
+		    {
+		        Console.Write(", ");
+		    }
+		    else
+		    {
+		        Console.WriteLine();
+		    }
+		}
+	    }
+	}
+    }
+}
