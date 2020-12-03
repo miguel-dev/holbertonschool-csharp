d1228c18b7f2aed960f30e125c1db0794b6c3fb4
miguel-dev <miguelacortes@zoho.com>
2020-12-03 12:26:16 -0500

    [Task] 5. Find the max

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+	int max = 0;
+        foreach (int integer in myList)
+	{
+	    if (integer > max)
+            {
+                max = integer;
+	    }
+	}
+	return max;
+    }
+}
