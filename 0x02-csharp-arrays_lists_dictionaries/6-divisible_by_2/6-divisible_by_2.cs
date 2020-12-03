ac9a3d8935a8edd107b3ca35eb753d5a0fc1670e
miguel-dev <miguelacortes@zoho.com>
2020-12-03 18:33:59 -0500

    [Task] 6. Only by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> list = new List<bool>();
+	foreach (int integer in myList)
+	{
+            if (integer % 2 == 0)
+            {
+                list.Add(true); 
+            }
+	    else
+	    {
+                list.Add(false);
+	    }
+        }
+	return list;
+    }
+}
