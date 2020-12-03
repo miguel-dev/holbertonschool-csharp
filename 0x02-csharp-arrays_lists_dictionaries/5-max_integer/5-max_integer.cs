6cc1a9273b92362bf9bfb0b75bcd23d99e28241c
miguel-dev <miguelacortes@zoho.com>
2020-12-03 12:31:30 -0500

    [Fixes] Evaluates if list is empty and in that case prints a message and returns -1

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -5,6 +5,12 @@ class List
 {
     public static int MaxInteger(List<int> myList)
     {
+	if (myList.Count == 0)
+	{
+            Console.WriteLine("List is empty");
+	    return -1;
+	}
+	    
 	int max = 0;
         foreach (int integer in myList)
 	{
