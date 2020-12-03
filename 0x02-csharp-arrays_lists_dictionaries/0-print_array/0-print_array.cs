a8b9b3d540e6d5a5114b2aa3f15534b3bb305f51
miguel-dev <miguelacortes@zoho.com>
2020-12-02 22:05:43 -0500

    [Task] 0. Print an array of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+	if(size < 0)
+	{
+	    Console.WriteLine("Size cannot be negative");
+	    return null;
+	}
+
+        int[] array = new int[size];
+	if (size == 0)
+	{
+            Console.WriteLine();
+	    return array;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = i;
+		Console.Write($"{i}", i);
+                if (i != size - 1)
+		{
+		    Console.Write(" ");
+		}
+		else
+		{
+                    Console.WriteLine();
+		}
+	}
+	return array;
+    }
+}
