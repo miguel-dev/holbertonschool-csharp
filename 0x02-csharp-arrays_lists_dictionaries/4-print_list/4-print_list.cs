1e792d182dfad02704ff13697d7b40e7c57954e2
miguel-dev <miguelacortes@zoho.com>
2020-12-03 00:08:19 -0500

    [Task] 4. Print a list of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+	if (size < 0)
+	{
+            Console.WriteLine("Size cannot be negative");
+	    return null;
+	}
+
+        List<int> list = new List<int>();
+	for (int i = 0; i < size; i++)
+	{
+	    list.Add(i);
+	    Console.Write(i);
+	    if (i != size - 1)
+	    {
+	        Console.Write(" ");
+	    }
+	}
+	Console.WriteLine();
+	return list;
+    }
+}
