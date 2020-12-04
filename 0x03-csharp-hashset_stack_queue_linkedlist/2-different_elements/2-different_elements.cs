77a174449b0791920b72720df9e42a7dbf167d46
miguel-dev <miguelacortes@zoho.com>
2020-12-04 17:19:53 -0500

    [Task] 2. Only differents

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        set1.SymmetricExceptWith(set2);
+        List<int> different = new List<int>(set1);
+        different.Sort();
+        return different;
+    }
+}
