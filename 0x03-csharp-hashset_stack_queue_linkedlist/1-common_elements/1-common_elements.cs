9ef237ffc2fd036a7dde131c490ada3fb5c56c31
miguel-dev <miguelacortes@zoho.com>
2020-12-04 00:20:20 -0500

    [Task] 1. Present in both

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        set1.IntersectWith(set2);
+        List<int> common = new List<int>(set1);
+        common.Sort();
+        return (common);
+    }
+}
