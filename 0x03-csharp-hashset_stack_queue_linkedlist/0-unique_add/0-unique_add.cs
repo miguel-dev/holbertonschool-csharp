75a7fbfd429d4837ca6882fc469dadc6d4313589
miguel-dev <miguelacortes@zoho.com>
2020-12-03 23:40:40 -0500

    [Task] 0. Unique addition

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> set = new HashSet<int>(myList);
+        int Sum = 0;
+        foreach (int element in set)
+        {
+            Sum += element;
+        }
+        return Sum;
+    }
+}
