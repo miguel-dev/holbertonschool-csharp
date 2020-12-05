4ae7055b5ed8027c7fc1550e25abbb502d93e68d
miguel-dev <miguelacortes@zoho.com>
2020-12-04 23:33:21 -0500

    [Task] 13. Best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bestKey = "None";
+        int bestValue = 0;
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (pair.Value > bestValue)
+            {
+                bestValue = pair.Value;
+                bestKey = pair.Key;
+            }
+        }
+        return bestKey;
+    }
+}
