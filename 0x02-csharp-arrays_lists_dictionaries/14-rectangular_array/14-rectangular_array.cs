4bb5175b11cd07afa7c4acf0388d9f4ad922fea2
miguel-dev <miguelacortes@zoho.com>
2020-12-05 13:07:31 -0500

    [Task] 14. Rectangular array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [,] array = new int[5, 5] {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}};
+        for (int rows = 0; rows < 5; rows++)
+        {
+            for (int columns = 0; columns < 5; columns++)
+            {
+                Console.Write(array[rows, columns]);
+                if (columns != 4)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
