3eacdb5d248a603691eeca2a32632f3fe7fd76a3
miguel-dev <miguelacortes@zoho.com>
2020-12-04 22:54:10 -0500

    [Task] 12. Print sorted dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keys = new List<string>(myDict.Keys);
+        keys.Sort();
+        int length = myDict.Count;
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine("{0}: {1}", keys[i], myDict[keys[i]]);
+        }
+    }
+}
