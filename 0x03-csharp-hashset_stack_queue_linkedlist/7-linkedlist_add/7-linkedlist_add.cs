5897f5213965b2fab0fa7a0a71fa391827f254f1
miguel-dev <miguelacortes@zoho.com>
2020-12-06 17:35:53 -0500

    [Task] 7. Add node to the beginning

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node;
+        node = myLList.AddFirst(n);
+        return node;
+    }
+}
