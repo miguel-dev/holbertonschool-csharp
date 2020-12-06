b172b4f887ce9737a85d0c47cc0a68d55c2e9fea
miguel-dev <miguelacortes@zoho.com>
2020-12-05 23:21:24 -0500

    [Task] 4. Queue it up

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int number = aQueue.Count;
+        bool contains = aQueue.Contains(search);
+        Console.WriteLine($"Number of items: {number}", number.ToString());
+
+        if (number == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            aQueue.Enqueue(newItem);
+            Console.WriteLine($"Queue contains \"{search}\": {contains}", search, contains.ToString());
+            return aQueue;
+        }
+
+        string first = aQueue.Peek();
+        Console.WriteLine($"First item: {first}", first);
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine($"Queue contains \"{search}\": {contains}", search, contains.ToString());
+
+        Queue<string> copy = new Queue<string>(aQueue);
+        if (contains)
+        {
+            string deleted = String.Empty;
+            foreach (string item in copy)
+            {
+                deleted = aQueue.Dequeue();
+                if (deleted == search)
+                {
+                    break;
+                }
+            }
+        }
+        return aQueue;
+    }
+}
