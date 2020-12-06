1b97d0def516297f4ef9440ae9a8d84cdb7a6e1e
miguel-dev <miguelacortes@zoho.com>
2020-12-05 20:22:47 -0500

    [Task] 3. Full stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int number = aStack.Count;
+        string top = aStack.Peek();
+        bool contains = aStack.Contains(search);
+        Console.WriteLine($"Number of items: {number}", number.ToString());
+        Console.WriteLine($"Top item: {top}", top);
+        Console.WriteLine($"Stack contains {search}: {contains}", search, contains.ToString());
+
+        Stack<string> copy = new Stack<string>(aStack);
+        if (contains)
+        {
+            string deleted = String.Empty;
+            foreach (string item in copy)
+            {
+                deleted = aStack.Pop();
+                
+                if (deleted == search)
+                {
+                    break;
+                }
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
