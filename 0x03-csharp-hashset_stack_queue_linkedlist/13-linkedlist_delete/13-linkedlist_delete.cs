ce1235e3a7322ca2d40fd07c3568d947d77ac3f3
miguel-dev <miguelacortes@zoho.com>
2020-12-07 00:15:58 -0500

    [Task] 13. Delete at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i = 0;
+        LinkedList<int> newLList = new LinkedList<int>(myLList);
+        foreach (int value in newLList)
+        {
+            if (i == index)
+            {
+                myLList.Remove(value);
+            }
+            i++;
+        }
+    }
+}
