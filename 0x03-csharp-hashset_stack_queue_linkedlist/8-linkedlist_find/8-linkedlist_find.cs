850f44ea69fab80624a71c397d2fdb10dd9e3308
miguel-dev <miguelacortes@zoho.com>
2020-12-06 20:09:10 -0500

    [Task] 8. Find value

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+	    foreach (int nodeValue in myLList)
+        {
+            if (nodeValue == value)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
