ee3be3965ce9156b8131ce46f875210c64ce1058
miguel-dev <miguelacortes@zoho.com>
2020-12-06 20:28:49 -0500

    [Task] 9. Pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> node = myLList.First;
+        myLList.Remove(node);
+        return node.Value;
+    }
+}
