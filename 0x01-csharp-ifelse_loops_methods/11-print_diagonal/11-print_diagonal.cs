259668f75cfa71ddfeca91dae5b9241d47103932
miguel-dev <miguelacortes@zoho.com>
2020-12-07 12:26:58 -0500

    [Task] 11. I feel like I am diagonally parked in a parallel universe

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+        {
+            Console.WriteLine();
+        }
+    
+        int j = 0;
+        int i = 0;
+        while (i < length)
+        {
+            j = 0;
+            while(j < i)
+            {
+                Console.Write(" ");
+                j++;
+            }
+            Console.WriteLine("\\");
+            i++;
+        }
+    }
+}
