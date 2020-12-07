557f892cf20e4baf42e988501acbdb2c6733d32f
miguel-dev <miguelacortes@zoho.com>
2020-12-06 22:32:32 -0500

    [Task] 12. Insert in order

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node = myLList.First.Next;
+        while (node != myLList.Last)
+        {
+            if (n > node.Previous.Value && n < node.Value)
+            {
+                return myLList.AddBefore(node, n);
+            }
+            node = node.Next;
+        }
+        return myLList.AddAfter(node, n);
+    }
+}
