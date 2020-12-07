435d17361ae8b4df9d8d8b72fef0e68a39c57fd8
miguel-dev <miguelacortes@zoho.com>
2020-12-06 21:17:32 -0500

    [Task] 10. Get node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach (int value in myLList)
+        {
+            if (index == n)
+            {
+                return value;
+            }
+            index++;
+        }
+        return 0;
+    }
+}
