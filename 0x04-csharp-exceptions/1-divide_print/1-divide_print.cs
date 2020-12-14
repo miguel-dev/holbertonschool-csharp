0a6d216e946bbf48d6543011ad33cd1df5abaec0
miguel-dev <miguelacortes@zoho.com>
2020-12-14 17:58:03 -0500

    [Task] 1. Integer division

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int q = 0;
+        try {
+            q = a/b;
+            Console.WriteLine($"{a} / {b} = {q}", a, b, a/b);
+        }
+        catch (DivideByZeroException) {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine($"{a} / {b} = {q}", a, 0, 0);
+        }
+    }
+}
