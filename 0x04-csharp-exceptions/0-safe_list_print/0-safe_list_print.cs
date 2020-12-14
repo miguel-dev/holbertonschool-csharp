6dd47597ce6ebf6231f9dab9231ff4f1a807843d
miguel-dev <miguelacortes@zoho.com>
2020-12-14 17:17:25 -0500

    [Task] 0. Safe list printing

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int numPrinted = 0;
+        for (int i = 0; i < n; i++)
+        {
+            try {
+                Console.WriteLine(myList[i]);
+                numPrinted++;
+            }
+            catch (ArgumentOutOfRangeException) {
+                return numPrinted;
+            }
+        }
+        return numPrinted;
+    }
+}
