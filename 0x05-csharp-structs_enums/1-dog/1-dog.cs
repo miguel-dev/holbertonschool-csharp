18808fb18270ee05f5f46eae0cfe3253868ffdbe
miguel-dev <miguelacortes@zoho.com>
2020-12-14 22:33:20 -0500

    [Task] 1. Chief Puppy Officer

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog
+{
+    public string name {get; set;}
+    public float age {get; set;}
+    public string owner {get; set;}
+    public Rating rating {get; set;}
+}
