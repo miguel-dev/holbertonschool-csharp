1836410a0417a64e9db5ee5f9236de711fc99159
miguel-dev <miguelacortes@zoho.com>
2020-12-14 23:13:58 -0500

    [Fixes] Removed set and get from members as well as the constructor

--- 0x05-csharp-structs_enums/1-dog/1-dog.cs
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -8,15 +8,8 @@ public enum Rating
 }
 public struct Dog
 {
-    public Dog (string Name, float Age, string Owner, Rating Rating)
-    {
-        name = Name;
-        age = Age;
-        owner = Owner;
-        rating = Rating;
-    }
-    public string name {get; set;}
-    public float age {get; set;}
-    public string owner {get; set;}
-    public Rating rating {get; set;}
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
 }
