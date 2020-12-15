00aa241b53b951486f96963ebce4a31158830ea9
miguel-dev <miguelacortes@zoho.com>
2020-12-14 23:23:19 -0500

    [Task] 2. A dog is the only thing on earth that loves you more than you love yourself

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog
+{
+    public Dog(string dogName, float dogAge, string dogOwner, Rating dogRating)
+    {
+        name = dogName;
+        age = dogAge;
+        owner = dogOwner;
+        rating = dogRating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
