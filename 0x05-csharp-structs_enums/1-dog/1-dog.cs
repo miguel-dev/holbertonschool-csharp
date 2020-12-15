cbda6733c6bcf60aa0d887867b28f61d94d43643
miguel-dev <miguelacortes@zoho.com>
2020-12-14 22:43:58 -0500

    [Fixes] Added constructor method

--- 0x05-csharp-structs_enums/1-dog/1-dog.cs
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -8,6 +8,13 @@ public enum Rating
 }
 public struct Dog
 {
+    public Dog (string name, float age, string owner, Rating rating)
+    {
+        name = name;
+        age = age;
+        owner = owner;
+        rating = rating;
+    }
     public string name {get; set;}
     public float age {get; set;}
     public string owner {get; set;}
