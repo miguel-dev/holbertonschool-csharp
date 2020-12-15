58303422182438596b1f414f39f2e57de2908e71
miguel-dev <miguelacortes@zoho.com>
2020-12-14 23:49:34 -0500

    [Fixes] Added .ToString() to the other attributes in case they are empty

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -22,6 +22,6 @@ public struct Dog
 
     public override string ToString()
     {
-        return string.Format("Dog Name: {0} \nAge: {1} \nOwner: {2} \nRating: {3}", name, age.ToString(), owner, rating.ToString());
+        return string.Format("Dog Name: {0} \nAge: {1} \nOwner: {2} \nRating: {3}", name.ToString(), age.ToString(), owner.ToString(), rating.ToString());
     }
 }
