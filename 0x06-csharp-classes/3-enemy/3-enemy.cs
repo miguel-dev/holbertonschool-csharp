40b04fd3372644be46598f329fc422a8cbaf6c22
miguel-dev <miguelacortes@zoho.com>
2020-12-15 21:28:40 -0500

    [Fixes] Changed // for /* */ for private field

--- 0x06-csharp-classes/3-enemy/3-enemy.cs
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -5,7 +5,7 @@ namespace Enemies
     /// <summary>Defines a zombie.</summary>
     public class Zombie
     {
-        // Gets or sets the health.
+        /* Gets or sets the health. */
         private int health {get; set;}
 
         /// <summary>Initializes a new instance of Zombie.</summary>
@@ -27,7 +27,7 @@ namespace Enemies
             }
         }
 
-        /// <summary>Returns the health of the Zombie instance</summary>
+        /// <summary>Returns the health of the Zombie instance.</summary>
         public int GetHealth()
         {
             return health;
