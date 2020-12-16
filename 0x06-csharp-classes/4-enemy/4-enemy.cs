899d9a44ec517c0928d5e75c835ca6056c01c35f
miguel-dev <miguelacortes@zoho.com>
2020-12-15 22:35:19 -0500

    [Task] 4. Hello, my name is

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a zombie.</summary>
+    public class Zombie
+    {
+        // Private field health.
+        private int health;
+
+        // Private field name.
+        private string name = "(No name)";
+
+        /// <summary>Public property Name that gets and sets name.</summary>
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie ()
+        {
+            health = 0;
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// <summary>Returns the health of the Zombie instance.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
