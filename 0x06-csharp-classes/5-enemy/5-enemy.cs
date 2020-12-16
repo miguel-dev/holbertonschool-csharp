931bb30f8bf45a8b1ef25455a685b8b705b0d0ce
miguel-dev <miguelacortes@zoho.com>
2020-12-15 22:50:21 -0500

    [Task] 5. Printing a zombie

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a zombie.</summary>
+    public class Zombie
+    {
+        // Private field health.
+        private int health;
+
+        // Private field name.
+        private string name = "(No name)";
+
+        /// <summary>Public property Name that gets and sets name.</summary>
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie ()
+        {
+            health = 0;
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// <summary>Returns the health of the Zombie instance.</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>Public ToString override method that prints Zombie stats</summary>
+        public override string ToString()
+        {
+            return String.Format("Zombie Name: {0} / Total Health: {1}", name, health.ToString());
+        }
+    }
+}
