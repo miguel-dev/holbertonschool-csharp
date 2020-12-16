d5384f669dab3bafcf52d004b594db1896bd3dfb
miguel-dev <miguelacortes@zoho.com>
2020-12-15 21:22:40 -0500

    [Task] 3. Zombie health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a zombie.</summary>
+    public class Zombie
+    {
+        // Gets or sets the health.
+        private int health {get; set;}
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie ()
+        {
+            health = 0;
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+
+        /// <summary>Returns the health of the Zombie instance</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
