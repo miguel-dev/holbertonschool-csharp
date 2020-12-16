3cb28d0ad1fc3328f96d7a8f0c5084c21bc1240f
miguel-dev <miguelacortes@zoho.com>
2020-12-15 21:10:23 -0500

    [Task] 2. Health validation

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a zombie.</summary>
+    public class Zombie
+    {
+        /// <summary>Gets or sets the health.</summary>
+        public int health {get; set;}
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie ()
+        {
+            health = 0;
+        }
+
+        /// <summary>Initializes a new instance of Zombie.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
