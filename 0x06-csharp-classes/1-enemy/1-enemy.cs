2c6a25670e26df5f1418c09a441f9a77c33f464f
miguel-dev <miguelacortes@zoho.com>
2020-12-15 20:55:19 -0500

    [Task] 1. Healthy competition

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a zombie.</summary>
+    public class Zombie
+    {
+        /// <summary>Gets or sets the health.</summary>
+        public int health {get; set;}
+
+        /// <summary>Initializes a new instance of Zombie</summary>
+        public Zombie ()
+        {
+            health = 0;
+        }
+    }
+}
