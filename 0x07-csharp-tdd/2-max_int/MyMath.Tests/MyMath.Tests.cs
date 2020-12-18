883aecbfd66e5318c298f70e017fa4d1ce938f7b
miguel-dev <miguelacortes@zoho.com>
2020-12-18 18:53:30 -0500

    [Task] 2. Max integer

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_Is_Empty()
+        {
+            List<int> emptyList = new List<int>();
+            Assert.AreEqual(0, Operations.Max(emptyList));
+        }
+
+        [Test]
+        public void Test_Max()
+        {
+            List<int> list = new List<int>() {1, 2, 3, 4};
+            Assert.AreEqual(4, Operations.Max(list));
+        }
+
+        [Test]
+        public void Test_Max_For_Negatives()
+        {
+            List<int> list = new List<int>() {-4, -3, -2, -1};
+            Assert.AreEqual(-1, Operations.Max(list));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>Contains a method to find the maximum number in a list</summary>
+    public class Operations
+    {
+        /// <summary>Finds the maximum in a list on integers</summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                return 0;
+            }
+
+            int max = nums[0];
+            foreach (int element in nums)
+            {
+                if (element > max)
+                {
+                    max = element;
+                }
+            }
+            return max;
+        }
+    }
+}
