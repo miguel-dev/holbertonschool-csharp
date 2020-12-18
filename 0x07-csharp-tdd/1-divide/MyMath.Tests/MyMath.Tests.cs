cd8acd0037c884444508aa815b04d6a064e3108d
miguel-dev <miguelacortes@zoho.com>
2020-12-17 23:48:02 -0500

    [Task] 1. Divide a matrix

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests_Matrix_Division
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_Null()
+        {
+            Assert.IsNull(Matrix.Divide(null, 5));
+        }
+
+        [Test]
+        public void Test_Division_By_Zero()
+        {
+            int[,] matrix = new int[3, 3] {{1, 2, 3}, {3, 4, 5}, {4, 5, 6}};
+            Assert.IsNull(Matrix.Divide(matrix, 0));
+        }
+
+        [Test]
+        public void Test_Division()
+        {
+            int[,] matrix = new int[3, 3] {{2, 4, 6}, {8, 10, 12}, {14, 16, 18}};
+            int[,] matrix_div = new int[3, 3] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+            Assert.AreEqual(matrix_div, Matrix.Divide(matrix, 2));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Contains a method for performing Matrix division.</summary>
+    public class Matrix
+    {
+        /// <summary>Divides elements of a Matrix.</summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    try
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Num cannot be 0");
+                        return null;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
