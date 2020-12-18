1a338d3d2a3485e42643a06de2dd6090e145cee7
miguel-dev <miguelacortes@zoho.com>
2020-12-18 17:26:55 -0500

    [Test] Added a test to check division for negative number

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -31,5 +31,13 @@ namespace MyMath.Tests
             int[,] matrix_div = new int[3, 3] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
             Assert.AreEqual(matrix_div, Matrix.Divide(matrix, 2));
         }
+
+        [Test]
+        public void Test_Division_By_Negative()
+        {
+            int[,] matrix = new int[3, 3] {{2, 4, 6}, {8, 10, 12}, {14, 16, 18}};
+            int[,] matrix_div = new int[3, 3] {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+            Assert.AreEqual(matrix_div, Matrix.Divide(matrix, -2));
+        }
     }
 }
