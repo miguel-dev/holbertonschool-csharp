ec643b426e23e5e52388486d5e192734fd7aa571
miguel-dev <miguelacortes@zoho.com>
2020-12-20 18:24:33 -0500

    [Task] 2. Only subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>Contains method to determine if a type derives from another.</summary>
+class Obj
+{
+    /// <summary>Checks if a Type inherits of another.</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
