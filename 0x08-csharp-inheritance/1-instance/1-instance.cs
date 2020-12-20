a73e40799914393d34147f5564d32feb60f90929
miguel-dev <miguelacortes@zoho.com>
2020-12-20 17:31:19 -0500

    [Task] 1. For instance

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Contains a method to check if an object is an instance of Array or an instance of
+/// a subclass of it.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if the object is an instance of, or if it's an instance of a class 
+    /// that inherits from Array.
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj) || obj.GetType().IsSubclassOf(typeof(Array)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
