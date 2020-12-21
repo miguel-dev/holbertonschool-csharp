216ebd9455136259ffae3be81b366b99f9e26832
miguel-dev <miguelacortes@zoho.com>
2020-12-20 23:39:55 -0500

    [Fixes] Added XML documentation

--- 0x08-csharp-inheritance/5-shape/5-shape.cs
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+/// <summary>Contains a method to calculate the area</summary>
 class Shape
 {
+    /// <summary>Calculates the area</summary>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
