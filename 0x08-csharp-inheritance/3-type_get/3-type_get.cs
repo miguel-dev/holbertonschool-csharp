f84ec554c6ae8f525739856f905df5e460e87e88
miguel-dev <miguelacortes@zoho.com>
2020-12-20 21:59:07 -0500

    [Task] 3. Type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+/// <summary>Contains a method that prints available properties and 
+/// methods of an object.
+/// </summary>
+class Obj
+{
+    /// <summary>Prints properties and methods of an object.</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo t_info = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> properties = t_info.DeclaredProperties;
+        IEnumerable<MethodInfo> methods = t_info.DeclaredMethods;
+
+        Console.WriteLine("{0} Properties:", t_info.Name);
+        
+        foreach (PropertyInfo p in properties)
+        {
+            Console.WriteLine(p.Name);
+        }
+
+        Console.WriteLine("{0} Methods:", t_info.Name);
+
+        foreach (MethodInfo m in methods)
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
