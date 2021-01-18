b46302e6eeb146fd28d8833de592efa434b80133
miguel-dev <miguelacortes@zoho.com>
2021-01-17 22:54:37 -0500

    [Fixes] Rounds magnitude to the nearest tenth

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,14 +7,17 @@ class VectorMath
     public static double Magnitude(double[] vector)
     {
         int length = vector.Length;
+        int magnitude;
 
         if (length == 2)
         {
-            return Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
+            magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
+            return Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
         else if (length == 3)
         {
-            return Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0) + Math.Pow(vector[2], 2.0));
+            magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0) + Math.Pow(vector[2], 2.0));
+            return magnitude = Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
         else
         {
