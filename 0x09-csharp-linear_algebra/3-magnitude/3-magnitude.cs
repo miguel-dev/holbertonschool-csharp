f91f726c784621a8533e90e218372c788f7bcff8
miguel-dev <miguelacortes@zoho.com>
2021-01-17 23:45:22 -0500

    [Fixes] Checks if array is null

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,12 +6,20 @@ class VectorMath
     /// <summary>Calculates the magnitude of a Vector</summary>
     public static double Magnitude(double[] vector)
     {
-        int length = vector.Length;
-        double magnitude = -1;
+        int length = 0;
+        double magnitude = 0;
 
+        if (vector == null)
+            return -1;
+
+        length = vector.Length;
+        
         if (length == 2)
         {
-            magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
+            try:
+                magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
+            except:
+
             return Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
         else if (length == 3)
@@ -21,7 +29,7 @@ class VectorMath
         }
         else
         {
-            return Math.Round(magnitude, 1, MidpointRounding.ToEven);
+            return -1;
         }
     }
 }
