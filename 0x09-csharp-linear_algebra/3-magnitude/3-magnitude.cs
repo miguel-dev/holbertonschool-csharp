fd2ac7fbc1ed29292e456ca425f7607723c25090
miguel-dev <miguelacortes@zoho.com>
2021-01-18 10:14:30 -0500

    [Fixes] Using two decimal places now

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,19 +6,22 @@ class VectorMath
     /// <summary>Calculates the magnitude of a Vector</summary>
     public static double Magnitude(double[] vector)
     {
-        double magnitude = -1;
+        double magnitude = 0;
         int length = vector.Length;
 
         if (length == 2)
         {
             magnitude = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
-            magnitude = Math.Round(magnitude, 1, MidpointRounding.ToEven);
+            return Math.Round(magnitude, 2, MidpointRounding.ToEven);
         }
         else if (length == 3)
         {
             magnitude = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2));
-            magnitude = Math.Round(magnitude, 1, MidpointRounding.ToEven);
+            return Math.Round(magnitude, 2, MidpointRounding.ToEven);
         }
-        return magnitude;
+        else
+        {
+            return -1;
+        }
     }
 }
