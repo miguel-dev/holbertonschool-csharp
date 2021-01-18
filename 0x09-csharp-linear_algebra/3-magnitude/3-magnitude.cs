827144a840eff26f4667f138b21144d2dbd30d95
miguel-dev <miguelacortes@zoho.com>
2021-01-17 23:53:48 -0500

    [Fixes] Removed the check for null

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,20 +6,13 @@ class VectorMath
     /// <summary>Calculates the magnitude of a Vector</summary>
     public static double Magnitude(double[] vector)
     {
-        int length = 0;
+        int length = vector.Length;
         double magnitude = 0;
 
-        if (vector == null)
-            return -1;
-
-        length = vector.Length;
-        
         if (length == 2)
         {
-            try:
-                magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
-            except:
 
+            magnitude = Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
             return Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
         else if (length == 3)
