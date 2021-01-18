93131451420b6648d1902140e98513602fbd714a
miguel-dev <miguelacortes@zoho.com>
2021-01-17 22:32:49 -0500

    [Task] 3. Magnitude #2

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int length = vector.Length;
+
+        if (length == 2)
+        {
+            return Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0));
+        }
+        else if (length == 3)
+        {
+            return Math.Sqrt(Math.Pow(vector[0], 2.0) + Math.Pow(vector[1], 2.0) + Math.Pow(vector[2], 2.0));
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
