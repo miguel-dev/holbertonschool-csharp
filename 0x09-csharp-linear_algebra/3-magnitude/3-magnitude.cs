5292557fdbbc61f618e55c87865b539256e0baf0
miguel-dev <miguelacortes@zoho.com>
2021-01-18 00:23:42 -0500

    [Fixes] Code refactor

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,22 +6,19 @@ class VectorMath
     /// <summary>Calculates the magnitude of a Vector</summary>
     public static double Magnitude(double[] vector)
     {
-        double magnitude = 0;
+        double magnitude = -1;
+        int length = vector.Length;
 
-        if (vector.Length == 2)
+        if (length == 2)
         {
-
             magnitude = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2));
-            return Math.Round(magnitude, 1, MidpointRounding.ToEven);
+            magnitude = Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
-        else if (vector.Length == 3)
+        else if (length == 3)
         {
             magnitude = Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2));
-            return Math.Round(magnitude, 1, MidpointRounding.ToEven);
+            magnitude = Math.Round(magnitude, 1, MidpointRounding.ToEven);
         }
-        else
-        {
-            return (-1);
-        }
+        return magnitude;
     }
 }
