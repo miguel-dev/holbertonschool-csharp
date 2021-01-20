1c901075d0eff594c0743fed5667319dcfcee23f
miguel-dev <miguelacortes@zoho.com>
2021-01-19 22:32:24 -0500

    [Task] 9. Vector-scalar multiplication #2

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        int length = vector.Length;
+        double[] result2d = new double[2];
+        double[] result3d = new double[3];
+        double[] res_error = {-1};
+
+        if (length == 2)
+        {
+            result2d[0] = vector[0] * scalar;
+            result2d[1] = vector[1] * scalar;
+            return result2d;
+        }
+        else if (length == 3)
+        {
+            result3d[0] = vector[0] * scalar;
+            result3d[1] = vector[1] * scalar;
+            result3d[2] = vector[2] * scalar;
+            return result3d;
+        }
+        else
+        {
+            return res_error;
+        }
+    }
+}
