ea5244bb32b9b6af3a37704c4d51f742eb0c3f79
miguel-dev <miguelacortes@zoho.com>
2021-01-24 18:15:45 -0500

    [Task] 22. Matrix shear #1

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] matrix_res = new double[2, 2];
+        double[,] matrix_err = {{-1}};
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return matrix_err;
+
+        if (direction != 'x' && direction != 'y')
+            return matrix_err;
+
+        if (direction == 'x')
+        {
+            matrix_res[0, 0] = matrix[0, 0] + (factor * matrix[0, 1]);
+            matrix_res[0, 1] = matrix[0, 1];
+            matrix_res[1, 0] = matrix[1, 0] + (factor * matrix[1, 1]);
+            matrix_res[1, 1] = matrix[1, 1];
+        }
+        else if (direction == 'y')
+        {
+            matrix_res[0, 0] = matrix[0, 0];
+            matrix_res[0, 1] = matrix[0, 1] + (factor * matrix[0, 0]);
+            matrix_res[1, 0] = matrix[1, 0];
+            matrix_res[1, 1] = matrix[1, 1] + (factor * matrix[1, 0]);
+        }
+        return matrix_res;
+    }
+}
