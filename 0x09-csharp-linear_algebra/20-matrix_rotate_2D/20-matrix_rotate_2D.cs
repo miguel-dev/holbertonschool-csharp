dbb3c2db1b10231433e92f9fc8c00f2e0d40eb05
miguel-dev <miguelacortes@zoho.com>
2021-01-23 21:46:46 -0500

    [Fixes] Rounded the outputs of the sin and cosine functions

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -8,12 +8,14 @@ class MatrixMath
         double[,] matrix_res = new double[2, 2];
 
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
             return matrix_err;
+        }
 
-        matrix_res[0, 0] = Math.Round((matrix[0, 0] * Math.Cos(angle)) - (matrix[0, 1] * Math.Sin(angle)));
-        matrix_res[0, 1] = Math.Round((matrix[0, 0] * Math.Sin(angle)) + (matrix[0, 1] * Math.Cos(angle)));
-        matrix_res[1, 0] = Math.Round((matrix[1, 0] * Math.Cos(angle)) - (matrix[1, 1] * Math.Sin(angle)));
-        matrix_res[1, 1] = Math.Round((matrix[1, 0] * Math.Sin(angle)) + (matrix[1, 1] * Math.Cos(angle)));
+        matrix_res[0, 0] = Math.Round((matrix[0, 0] * Math.Round(Math.Cos(angle))) - (matrix[0, 1] * Math.Round(Math.Sin(angle))));
+        matrix_res[0, 1] = Math.Round((matrix[0, 0] * Math.Round(Math.Sin(angle))) + (matrix[0, 1] * Math.Round(Math.Cos(angle))));
+        matrix_res[1, 0] = Math.Round((matrix[1, 0] * Math.Round(Math.Cos(angle))) - (matrix[1, 1] * Math.Round(Math.Sin(angle))));
+        matrix_res[1, 1] = Math.Round((matrix[1, 0] * Math.Round(Math.Sin(angle))) + (matrix[1, 1] * Math.Round(Math.Cos(angle))));
         return matrix_res;
     }
 }
