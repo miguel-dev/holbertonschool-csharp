c0c275a57af2dcd488b44bd8ede01d37bc141aa2
miguel-dev <miguelacortes@zoho.com>
2021-01-23 21:27:51 -0500

    [Fixes] Only uses the standard formula for matrix rotation

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -10,22 +10,10 @@ class MatrixMath
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return matrix_err;
 
-        if (angle > 0)
-        {
-            matrix_res[0, 0] = (matrix[0, 0] * Math.Cos(angle)) - (matrix[0, 1] * Math.Sin(angle));
-            matrix_res[0, 1] = (matrix[0, 0] * Math.Sin(angle)) + (matrix[0, 1] * Math.Cos(angle));
-            matrix_res[1, 0] = (matrix[1, 0] * Math.Cos(angle)) - (matrix[1, 1] * Math.Sin(angle));
-            matrix_res[1, 1] = (matrix[1, 0] * Math.Sin(angle)) + (matrix[1, 1] * Math.Cos(angle));
-            return matrix_res;
-        }
-        else if (angle < 0)
-        {
-            matrix_res[0, 0] = (matrix[0, 0] * Math.Cos(angle)) + (matrix[0, 1] * Math.Sin(angle));
-            matrix_res[0, 1] = (matrix[0, 0] * Math.Sin(angle)) + (matrix[0, 1] * Math.Cos(angle));
-            matrix_res[1, 0] = (matrix[1, 0] * Math.Cos(angle)) + (matrix[1, 1] * Math.Sin(angle));
-            matrix_res[1, 1] = -(matrix[1, 0] * Math.Sin(angle)) + (matrix[1, 1] * Math.Cos(angle));
-            return matrix_res;
-        }
+        matrix_res[0, 0] = Math.Round((matrix[0, 0] * Math.Cos(angle)) - (matrix[0, 1] * Math.Sin(angle)));
+        matrix_res[0, 1] = Math.Round((matrix[0, 0] * Math.Sin(angle)) + (matrix[0, 1] * Math.Cos(angle)));
+        matrix_res[1, 0] = Math.Round((matrix[1, 0] * Math.Cos(angle)) - (matrix[1, 1] * Math.Sin(angle)));
+        matrix_res[1, 1] = Math.Round((matrix[1, 0] * Math.Sin(angle)) + (matrix[1, 1] * Math.Cos(angle)));
         return matrix_res;
     }
 }
