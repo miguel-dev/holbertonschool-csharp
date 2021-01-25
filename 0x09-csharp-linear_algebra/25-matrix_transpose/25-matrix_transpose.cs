f0096a1a7eef917d27b90f1045f80c9a93b822b8
miguel-dev <miguelacortes@zoho.com>
2021-01-24 22:45:23 -0500

    [Task] 23. Transpose matrix

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+        double[,] transpose = new double[columns, rows];
+
+        if (matrix.Length == 0)
+            return matrix;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                transpose[j, i] = matrix[i, j];
+            }
+        }
+        return transpose;
+    }
+}
