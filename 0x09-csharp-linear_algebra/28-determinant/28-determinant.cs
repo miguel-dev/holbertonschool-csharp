7ace31644b85aec9f56e4e0c2aeba9127c6ef7ea
miguel-dev <miguelacortes@zoho.com>
2021-01-25 00:42:24 -0500

    [Task] 26. Determinant #3

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+        double determinant = 0;
+
+        if (rows == 2 && columns == 2)
+        {
+            determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+            return determinant;
+        }
+        else if (rows == 3 && columns == 3)
+        {
+            determinant = (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) + (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]) - (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) - (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) - (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
+            return determinant;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
