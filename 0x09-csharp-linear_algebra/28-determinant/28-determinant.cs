e95e1b93eee6444f7df5b53059efb668aa250718
miguel-dev <miguelacortes@zoho.com>
2021-01-25 00:45:49 -0500

    [Fixes] Rounds determinants

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -11,12 +11,12 @@ class MatrixMath
         if (rows == 2 && columns == 2)
         {
             determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
-            return determinant;
+            return Math.Round(determinant);
         }
         else if (rows == 3 && columns == 3)
         {
             determinant = (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) + (matrix[0, 2] * matrix[1, 0] * matrix[2, 1]) - (matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) - (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) - (matrix[0, 0] * matrix[1, 2] * matrix[2, 1]);
-            return determinant;
+            return Math.Round(determinant);
         }
         else
         {
