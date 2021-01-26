705274ef3c1972ce84bc0f69847ac4131bfddd84
miguel-dev <miguelacortes@zoho.com>
2021-01-25 21:58:43 -0500

    [Task] 31. Inverse #2

---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double[,] inverse = new double[2, 2];
+        double determinant = 0;
+        double[,] error = {{-1}};
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return error;
+        }
+
+        determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+
+        if (determinant == 0)
+        {
+            return error;
+        }
+
+        inverse[0, 0] = matrix[1, 1] / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+        inverse[0, 1] = -matrix[0, 1] / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+        inverse[1, 0] = -matrix[1, 0] / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+        inverse[1, 1] = matrix[0, 0] / ((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]));
+
+        return inverse;
+    }
+}
